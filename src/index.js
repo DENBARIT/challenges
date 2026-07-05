5e1b2bba0f9949ade6d764acce374ec61e21079e
Your Name <leulgebremariam1995@gmail.com>
2026-07-05 15:17:41 +0300

    Challenge initalization of flashcards

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot
  } from "react-dom/client";
 
- import CurrentApp from "./Part6_Challenges/Date_Counter"
+ import CurrentApp from "./FlashCards/FlashCard"
 
  const rootElement=document.getElementById("root");
  const root=createRoot(rootElement);
