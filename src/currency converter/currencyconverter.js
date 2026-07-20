750a0934eda7cf15dec9ad81030cfd98a8c68898
Your Name <leulgebremariam1995@gmail.com>
2026-07-20 12:24:10 +0300

    feat: optimize currency fetch with safeguards and abort controller

--- src/currency converter/currencyconverter.js
+++ src/currency converter/currencyconverter.js
@@ -6,38 +6,56 @@ APP() {
    const [convertedAmount, setConvertedAmount] = useState("");
     const [baseCurrency, setBaseCurrency] = useState("USD");
     const [quoteCurrency, setQuoteCurrency] = useState("EUR");
+    const [isLoading, setIsLoading] = useState(false);
 useEffect(() => {
-  
-    async function getData(){
-        const response=await fetch(`https://api.frankfurter.dev/v2/rate/${baseCurrency}/${quoteCurrency}`);
+    if(!amount||isNaN(amount)){
+        setConvertedAmount("");
+        return;
+    }
+     if (baseCurrency === quoteCurrency) {
+      setConvertedAmount(Number(amount).toFixed(2));
+      return;
+    }
+const abortController = new AbortController();
+    async function convert(){
+        try{
+            setIsLoading(true);
+        const response=await fetch(`https://api.frankfurter.dev/v2/rate/${baseCurrency}/${quoteCurrency}`,{signal: abortController.signal});
         const data=await response.json();
 const exchangeRate = data.rate;
 const amountToConvert = (Number(amount)*exchangeRate).toFixed(2);
 setConvertedAmount(amountToConvert);
-    
-    }getData();
+    setIsLoading(false);
+    }catch(error){
+if(error.name !== "AbortError"){
+    console.error("Fetch error: ", error);
+          setIsLoading(false);
+    }}}
+    convert();
   return function cleanup() {
+    abortController.abort();
     setConvertedAmount("");
   };
   
-}, [baseCurrency, quoteCurrency]);
+}, [amount,baseCurrency, quoteCurrency]);
 
     return(
     <div>
-        <input type="text" placeholder="Enter amount"  value={amount} onChange={(e) => setAmount(e.target.value)} />
+        <input type="text" placeholder="Enter amount"  value={amount} onChange={(e) => setAmount(e.target.value)} disabled={isLoading} />
         <select value={baseCurrency} onChange={(e) => setBaseCurrency(e.target.value)}>
             <option value="USD">USD</option>
             <option value="EUR">EUR</option>
             <option value="CAD">CAD</option>
             <option value="INR">INR</option>
         </select>
-          <select value={quoteCurrency} onChange={(e) => setQuoteCurrency(e.target.value)}>
+          <select value={quoteCurrency} onChange={(e) => setQuoteCurrency(e.target.value) } disabled={isLoading}>
             <option value="USD">USD</option>
             <option value="EUR">EUR</option>
             <option value="CAD">CAD</option>
             <option value="INR">INR</option>
         </select>
-        <p>Output: {convertedAmount}</p>
+        {convertedAmount &&
+        <p>Output: {convertedAmount} {quoteCurrency}</p>}
     </div>
   
 )
